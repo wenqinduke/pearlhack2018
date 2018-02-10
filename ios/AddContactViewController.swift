fe1f4487d949e27d1f87c4607fb2a5972613875a
Wenqin Wang <wenqin.wang@duke.edu>
2018-02-10 13:16:45 -0500

    add contact

---
+++ ios/AddContactViewController.swift
@@ -0,0 +1,60 @@
+//
+//  AddContactViewController.swift
+//  ios
+//
+//  Created by Wenqin Wang on 2/10/18.
+//  Copyright © 2018 Yiqin Zhou. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+class AddContactViewController: UIViewController {
+
+    @IBAction func Addcontact(_ sender: Any) {
+        let contact = CNMutableContact()
+        //contact.imageData = NSData() // The profile picture as a NSData object
+        
+        contact.givenName = "John"
+        contact.familyName = "Appleseed"
+        
+        let homeEmail = CNLabeledValue(label:CNLabelHome, value:"john@example.com")
+        let workEmail = CNLabeledValue(label:CNLabelWork, value:"j.appleseed@icloud.com")
+        contact.emailAddresses = [homeEmail, workEmail]
+        
+        let homeAddress = CNMutablePostalAddress()
+        homeAddress.street = "1 Infinite Loop"
+        homeAddress.city = "Cupertino"
+        homeAddress.state = "CA"
+        homeAddress.postalCode = "95014"
+        contact.postalAddresses = [CNLabeledValue(label:CNLabelHome, value:homeAddress)]
+ 
+        let store = CNContactStore()
+        let saveRequest = CNSaveRequest()
+        saveRequest.addContact(contact, toContainerWithIdentifier:nil)
+        try! store.executeSaveRequest(saveRequest)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
